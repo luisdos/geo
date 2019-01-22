5c94b53d10c29944a0591ce8a9eaede4dc9708df
silvia.romero <luis.flores@caffenio.com>
2019-01-22 16:17:58 -0700

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mensaje: "",
+      soportado: true,
+      lat: 0,
+      lon: 0,
+      userAgent: ""
+    }
+  }
+
+  componentWillMount() {
+    let state = {...this.state};
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((posicion) => {
+        state.lat = posicion.coords.latitude;
+        state.lon = posicion.coords.longitude;
+        state.userAgent = navigator.userAgent;
+        this.setState(state);
+      });
+    } else {
+      state.soportado = false;
+      state.mensaje = "Geolocalización no es soportado por este navegador.";
+      this.setState(state);
+    }
+
+
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {
+            this.state.soportado 
+            ? (
+              <div>
+                <p>Latitud: {this.state.lat}</p> 
+                <p>Longitud: {this.state.lon}</p> 
+                <p>User Agent: {this.state.userAgent}</p> 
+              </div> 
+              )
+            : ( <div>this.state.mensaje</div> )
+          }
+          
         </header>
       </div>
     );
